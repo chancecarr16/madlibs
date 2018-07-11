57e315087bdf5ba8e3768c1f1752a5138de50c99
student <chancecarr1@gmail.com>
2018-07-11 11:48:22 -0600

    Made header component

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div classname="header">
+            <h1>Madlibs</h1>
+            <p>text</p>
+        </div>
+    )
+}
+
+export default Header;
