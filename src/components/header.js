ef4b90cede1e8ab169c1c0d39faff8e766f286ee
student <chancecarr1@gmail.com>
2018-07-23 11:55:34 -0600

    Added first two grids

--- src/components/header.js
+++ src/components/header.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Header = () => {
     return (
         <div className="header">
-            <h1>Madlibs</h1>
-            <p>text</p>
+            <h1>Bottega Madlibs</h1>
+            <p>Fill out the fields below and click the generate button to see the Madlib story.</p>
         </div>
     )
 }
